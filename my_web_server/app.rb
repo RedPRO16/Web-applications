3b417fee84e7daa3b4d56bfadfa8dc0d12148a39
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2022-08-09 12:26:22 +0100

    first commit

--- my_web_server/app.rb
+++ my_web_server/app.rb
@@ -1,19 +1,27 @@
 require 'sinatra/base'
-
+require 'sinatra/reloader'
 
 class Application < Sinatra::Base
   # This allows the app code to refresh
   # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
 
   get '/' do
-    'Hello World!'
-
-    return 'Hello World!'
-  end
-
-  get '/posts' do
-   return 'A list of post'
 
   end
+
+  get '/hello' do
+    @name = params[:name]
+    return erb(:index)
+  end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+    "Thanks #{name}, you sent this message: #{message}"
+  end
+
 
 end
