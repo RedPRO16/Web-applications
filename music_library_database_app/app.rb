75e22bc36d0b4c5a8e4eaa685df91c7f8aee1747
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2022-08-10 10:58:12 +0100

    step 4 complete

--- music_library_database_app/app.rb
+++ music_library_database_app/app.rb
@@ -13,4 +13,25 @@ class Application < Sinatra::Base
     also_reload 'lib/album_repository'
     also_reload 'lib/artist_repository'
   end
+
+  get '/artists' do
+    repo =  ArtistRepository.new
+    artists = repo.all
+    response = artists.map { |artist| artist.name}.join(', ')
+
+  end
+
+  post '/artists' do
+    new_artist = Artist.new
+    new_artist.id = params['id'].to_i
+    new_artist.name = params['name']
+    new_artist.genre = params['genre']
+      repo = ArtistRepository.new
+      repo.create(new_artist)
+  end
+
+  get '/artists' do
+    repo =  ArtistRepository.new
+    artists = repo.all
+  end
 end
--- music_library_database_app/spec/integration/application_spec.rb
+++ music_library_database_app/spec/integration/application_spec.rb
@@ -2,6 +2,19 @@ require "spec_helper"
 require "rack/test"
 require_relative '../../app'
 
+
+def reset_albums_table
+  seed_sql = File.read('spec/seeds/albums_seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+  connection.exec(seed_sql)
+end
+
+describe Application do
+  before(:each) do
+    reset_albums_table
+  end
+end
+
 describe Application do
   # This is so we can use rack-test helper methods.
   include Rack::Test::Methods
@@ -10,4 +23,33 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context "GET /artists" do
+    it 'returns 200 OK' do
+      # Assuming the post with id 1 exists.
+      response = get('/artists', names: 'Pixies, ABBA, Taylor Swift, Nina Simone, Kiasmos')
+      expected_response = 'Pixies, ABBA, Taylor Swift, Nina Simone, Kiasmos'
+      expect(response.status).to eq (200)
+      expect(response.body).to eq (expected_response)
+    end
+  end
+
+  context "POST /artists" do
+    it 'returns 200 OK' do
+      # Assuming the post with id 1 exists.
+      response = post('/artists', name: 'Wild nothing', genre: 'Indie')
+
+      expect(response.status).to eq (200)
+      expect(response.body).to eq ('')
+    end
+  end
+
+  context "GET /artists" do
+    it 'returns 200 OK with include new artist' do
+      # Assuming the post with id 1 exists.
+      response = get('/artists')
+
+      expect(response.status).to eq (200)
+      expect(response.body).to include('Wild nothing')
+    end
+  end
 end
